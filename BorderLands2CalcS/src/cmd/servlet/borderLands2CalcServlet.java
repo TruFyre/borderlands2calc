9e0420fa0a372dc443ad01cb832d59182eb545ef
C Devers <cmdaese@gmail.com>
2013-03-15 23:40:21 -0500

    Added basic calculator logic.  I just wanted to see something work before bed...

--- BorderLands2CalcS/src/cmd/servlet/borderLands2CalcServlet.java
+++ BorderLands2CalcS/src/cmd/servlet/borderLands2CalcServlet.java
@@ -10,6 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Parser;
+
+import cmd.borderLands2Calc.Calculator;
+
 
 /**
  * Servlet implementation class borderLands2CalcServlet
@@ -49,16 +53,36 @@ public class borderLands2CalcServlet extends HttpServlet {
 
 		System.out.println("BorderLands2Calc doGet called.");
 		
+		String input = request.getParameter("inputValues");
+		
+		System.out.println("Input values = " + input);
+		
+		String[] strings = input.split(" ");
+		
+		float damage = Float.parseFloat(strings[0]);
+		float fireRate = Float.parseFloat(strings[1]);
+		float reloadTime = Float.parseFloat(strings[2]);
+		float clipSize = Float.parseFloat(strings[3]);
+		float ammoPerShot = Float.parseFloat(strings[4]);
+		
+		
+		Calculator calc = new Calculator();
+		float trueDps = calc.trueDps(damage, fireRate, reloadTime, clipSize, ammoPerShot);
+		
+		
 	    response.setContentType("text/html");
 	    
 	    PrintWriter out = response.getWriter();
+	    
 	    out.println("<html>");
 	    out.println("<body>");
 	    out.println("<head>");
-	    out.println("<title>First Example</title>");
+	    out.println("<title>BorderLands 2 Calculator</title>");
 	    out.println("</head>");
 	    out.println("<body>");
-	    out.println("<h1>Hello World!</h1>");
+	    out.println("<h1>Result:</h1>");
+	    out.println("True DPS = " + trueDps + "<br/><br/>");
+	    out.println("<a href='./index.html'>Go back</a>");
 	    out.println("</body>");
 	    out.println("</html>");
 		
@@ -68,8 +92,11 @@ public class borderLands2CalcServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		System.out.println("BorderLands2Calc doPost called.");
+		
+		doGet(request, response);
+		
 	}
 
 }
