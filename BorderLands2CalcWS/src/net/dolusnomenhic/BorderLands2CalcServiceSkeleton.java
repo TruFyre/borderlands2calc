8d9056f9ad805efee9d5f5589e98bc1252fe7603
C Devers <cmdaese@gmail.com>
2014-05-07 08:26:07 -0500

    Updated Skeleton so that service actually calls calculator and does not throw an exception.

--- BorderLands2CalcWS/src/net/dolusnomenhic/BorderLands2CalcServiceSkeleton.java
+++ BorderLands2CalcWS/src/net/dolusnomenhic/BorderLands2CalcServiceSkeleton.java
@@ -6,6 +6,8 @@
  * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
  */
     package net.dolusnomenhic;
+
+import cmd.borderLands2Calc.Calculator;
     /**
      *  BorderLands2CalcServiceSkeleton java skeleton for the axisService
      */
@@ -19,13 +21,15 @@
              * @return trueDpsResponse 
          */
         
-                 public net.dolusnomenhic.TrueDpsResponse calcTrueDps
-                  (
-                  net.dolusnomenhic.TrueDpsRequest trueDpsRequest
-                  )
+                 public net.dolusnomenhic.TrueDpsResponse calcTrueDps(net.dolusnomenhic.TrueDpsRequest trueDpsRequest)
             {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#calcTrueDps");
+                Calculator calculator = new Calculator();
+                
+                TrueDpsResponse trueDpsResponse = new TrueDpsResponse();
+                
+                trueDpsResponse.setTrueDps(1.0f);
+                
+                return trueDpsResponse;
         }
      
     }
