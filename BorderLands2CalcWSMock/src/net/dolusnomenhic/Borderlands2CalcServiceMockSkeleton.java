c946dd586ae40d56f015300557d31c68b72ff315
Christopher Devers <christopherdevers@Silver.local>
2015-04-26 10:17:47 -0500

    Added demonstration service for mocking

---
+++ BorderLands2CalcWSMock/src/net/dolusnomenhic/Borderlands2CalcServiceMockSkeleton.java
@@ -0,0 +1,31 @@
+
+/**
+ * Borderlands2CalcServiceMockSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
+ */
+    package net.dolusnomenhic;
+    /**
+     *  Borderlands2CalcServiceMockSkeleton java skeleton for the axisService
+     */
+    public class Borderlands2CalcServiceMockSkeleton implements Borderlands2CalcServiceMockSkeletonInterface{
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param trueDpsRequest0 
+             * @return trueDpsResponse1 
+         */
+        
+                 public net.dolusnomenhic.TrueDpsResponse calcTrueDps(net.dolusnomenhic.TrueDpsRequest trueDpsRequest0)
+            {
+                	 TrueDpsResponse trueDpsResponse = new TrueDpsResponse();
+                	 trueDpsResponse.setTrueDps(1337.0f);
+                	 return trueDpsResponse;
+                	 
+            }
+     
+    }
+    
---
+++ BorderLands2CalcWSMock/src/net/dolusnomenhic/Borderlands2CalcServiceMockSkeletonInterface.java
@@ -0,0 +1,29 @@
+
+/**
+ * Borderlands2CalcServiceMockSkeletonInterface.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
+ */
+    package net.dolusnomenhic;
+    /**
+     *  Borderlands2CalcServiceMockSkeletonInterface java skeleton interface for the axisService
+     */
+    public interface Borderlands2CalcServiceMockSkeletonInterface {
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                    * @param trueDpsRequest
+         */
+
+        
+                public net.dolusnomenhic.TrueDpsResponse calcTrueDps
+                (
+                  net.dolusnomenhic.TrueDpsRequest trueDpsRequest
+                 )
+            ;
+        
+         }
+    
